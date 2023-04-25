73cfd867cf1d457bcecc3e427280ac3f1ef404c0
Carles <carlesmartif@gmail.com>
2023-04-25 17:32:01 +0200

    Animación implementada para la casilla salto

--- Assets/LevelsAssets/Jump/Jump.cs
+++ Assets/LevelsAssets/Jump/Jump.cs
@@ -6,18 +6,28 @@ public class Jump : MonoBehaviour
 {
     public float jumpForceY = 10f;
     public float jumpForceX = -3.5f;
+    private Animator animator;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Animator playerAnimator = collision.gameObject.GetComponent<Animator>(); // Obtén el Animator del objeto "Player"
             if (playerRigidbody != null)
             {
+                animator = GetComponent<Animator>();
+                animator.SetTrigger("jump");
+
+                
                 playerRigidbody.constraints = RigidbodyConstraints.None;
                 playerRigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
                 playerRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
                 Vector3 force = new Vector3(jumpForceX, jumpForceY, 0f);
                 playerRigidbody.AddForce(force, ForceMode.Impulse);
+                
+
+
             }
         }
     }
