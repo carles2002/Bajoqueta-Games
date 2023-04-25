0e827b641302b99f33f1c42a72cc2a5945703f6a
Carles <cmarfem@upv.edu.es>
2023-04-25 21:15:39 +0200

    Salto 1 hecho
    Va un poco locuelo pero la base esta y es bastante usable

--- Assets/LevelsAssets/Jump/Jump.cs
+++ Assets/LevelsAssets/Jump/Jump.cs
@@ -5,23 +5,47 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     public GameObject player; // Agrega una variable pública para el objeto "Player"
-
+    public GameObject objectToMove; // Agrega una variable pública para el objeto que quieres mover
+    public float objectMoveDistance = 1f; // Agrega una variable pública para la distancia que se moverá el objeto
+    public RuntimeAnimatorController newAnimatorController; // Agrega una variable pública para el nuevo AnimatorController
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
         {
-
+            Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
             Animator playerAnimator = player.GetComponent<Animator>(); // Obtén el Animator del objeto "Player"
 
             Debug.Log("Entrado________________________");
 
-        
-                playerAnimator.SetTrigger("jump"); // Usa el Animator del objeto "Player"
-            
+            ChangeAnimatorController(playerAnimator);
+            MoveObjectUp(objectToMove, objectMoveDistance);
+
+            // playerAnimator.SetTrigger("jump"); // Usa el Animator del objeto "Player"
+        }
+    }
+
+    private void ChangeAnimatorController(Animator animator)
+    {
+        if (newAnimatorController != null)
+        {
+            animator.runtimeAnimatorController = newAnimatorController;
+        }
+        else
+        {
+            Debug.LogWarning("No se ha asignado un nuevo AnimatorController. Por favor, asigna uno en el Inspector.");
+        }
+    }
+
+    private void MoveObjectUp(GameObject objectToMove, float moveDistance)
+    {
+        if (objectToMove != null)
+        {
+            objectToMove.transform.position += new Vector3(0, moveDistance, 0);
+        }
+        else
+        {
+            Debug.LogWarning("No se ha asignado un objeto para mover. Por favor, asigna uno en el Inspector.");
         }
     }
 }
-
-
-
--- Assets/LevelsAssets/Jump/Jump2.cs
+++ Assets/LevelsAssets/Jump/Jump2.cs
@@ -4,24 +4,51 @@ using UnityEngine;
 
 public class Jump2 : MonoBehaviour
 {
-    public float jumpForceY = 10f;
-    public float jumpForceX = 0f;
-    public float jumpForceZ = -1.9f;
+    public GameObject player; // Agrega una variable pública para el objeto "Player"
+    public RuntimeAnimatorController newAnimatorController; // Agrega una variable pública para el nuevo AnimatorController
+    public float yOffset = 2f; // Agrega una variable pública para la cantidad de unidades que el personaje se moverá hacia arriba
+    public float platformYOffset = -1f; // Agrega una variable pública para la cantidad de unidades que la plataforma se moverá hacia abajo
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
             Rigidbody playerRigidbody = collision.gameObject.GetComponent<Rigidbody>();
-            if (playerRigidbody != null)
-            {
-                playerRigidbody.constraints = RigidbodyConstraints.None;
-                playerRigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
-                playerRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
-                Vector3 force = new Vector3(jumpForceX, jumpForceY, jumpForceZ);
-                playerRigidbody.AddForce(force, ForceMode.Impulse);
-            }
+            Animator playerAnimator = player.GetComponent<Animator>(); // Obtén el Animator del objeto "Player"
+
+            Debug.Log("Entrado________________________");
+
+            ChangeAnimatorController(playerAnimator);
+            MovePlayerAboveObject(player.transform, yOffset);
+            MovePlatformDown(transform, platformYOffset);
+
+           
         }
     }
 
+    private void ChangeAnimatorController(Animator animator)
+    {
+        if (newAnimatorController != null)
+        {
+            animator.runtimeAnimatorController = newAnimatorController;
+        }
+        else
+        {
+            Debug.LogWarning("No se ha asignado un nuevo AnimatorController. Por favor, asigna uno en el Inspector.");
+        }
+    }
+
+    private void MovePlayerAboveObject(Transform playerTransform, float yOffset)
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.y += yOffset;
+        playerTransform.position = newPosition;
+    }
+
+    private void MovePlatformDown(Transform platformTransform, float platformYOffset)
+    {
+        Vector3 newPosition = platformTransform.position;
+        newPosition.y += platformYOffset;
+        platformTransform.position = newPosition;
+    }
 }
