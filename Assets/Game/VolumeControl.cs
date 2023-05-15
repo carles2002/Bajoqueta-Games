16758e0a6550f01586a753140ce4bf450c4a75ab
Carles <cmarfem@upv.edu.es>
2023-05-15 17:27:23 +0200

    Volumen V1 en lvl1

--- Assets/Game/VolumeControl.cs
+++ Assets/Game/VolumeControl.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using System.Collections.Generic; // Necesitarás esta biblioteca para usar List<>.
 
 public class VolumeControl : MonoBehaviour
 {
     public AudioMixer mixer;
     public AudioSource audioSource;
-
-
-
+    public List<Sound> soundScripts; // Lista de referencias a los scripts Sound.
 
     public void OnScrollbarValueChanged(float value)
     {
@@ -18,12 +17,18 @@ public class VolumeControl : MonoBehaviour
         }
     }
 
-
     public void SetLevel(float sliderValue)
     {
-        float volume = Mathf.Lerp(-20, 0, sliderValue);
+        float volume = Mathf.Lerp(-25, -5, sliderValue);
         mixer.SetFloat("MusicVol", volume);
 
-        
+        // Cambia el volumen en todos los scripts Sound.
+        foreach (Sound soundScript in soundScripts)
+        {
+            if (soundScript != null)
+            {
+                soundScript.volumen = Mathf.Pow(10, volume / 20); // Convertimos el volumen de dB a una escala lineal.
+            }
+        }
     }
 }
