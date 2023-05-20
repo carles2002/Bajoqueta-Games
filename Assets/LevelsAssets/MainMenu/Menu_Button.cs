2a215b361574fac2c059519e6377ab025f9d6e40
Carles <cmarfem@upv.edu.es>
2023-05-20 10:19:01 +0200

    Animación de Poly en el Menú 0.1
    Se ha implementado la animación de Poly cuando se presiona el botón de Start

--- Assets/LevelsAssets/MainMenu/Menu_Button.cs
+++ Assets/LevelsAssets/MainMenu/Menu_Button.cs
@@ -6,7 +6,48 @@ using UnityEngine.Events;
 public class Menu_Button : MonoBehaviour
 {
     public UnityEvent unityEvent = new UnityEvent();
-    private void OnMouseUpAsButton() {
+    public bool isStart = false;
+
+    public GameObject objectToAnimate; // El objeto que quieres animar
+    private Animator animator;
+
+    // Asegúrate de que tu Trigger se llama "PolyJump" en tu Animator Controller
+    private readonly string polyJumpTrigger = "PolyJump";
+
+    private void Start()
+    {
+        // Recupera el componente Animator del objeto que quieres animar
+        if (objectToAnimate != null)
+        {
+            animator = objectToAnimate.GetComponent<Animator>();
+            
+        }
+        
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (isStart)
+        {
+            if (animator != null)
+            {
+                Debug.Log("LLEGADO");
+                animator.SetTrigger(polyJumpTrigger);
+            }
+           
+        }
+
+        // Inicia la corrutina para invocar el evento después de un retraso de 5 segundos
+        StartCoroutine(InvokeEventAfterDelay(1.0f));
+    }
+
+    IEnumerator InvokeEventAfterDelay(float delay)
+    {
+        // Espera el número especificado de segundos
+        yield return new WaitForSeconds(delay);
+
+        // Después de la espera, invoca el evento
         unityEvent.Invoke();
     }
+
 }
