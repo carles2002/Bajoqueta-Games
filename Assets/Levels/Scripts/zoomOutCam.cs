a9da165ca3e0a030f2f97b200b68fe13bfa6cb33
Carles <cmarfem@upv.edu.es>
2023-06-04 19:06:31 +0200

    Animación de cámara al inicio de la escena implementada

---
+++ Assets/Levels/Scripts/zoomOutCam.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class zoomOutCam : MonoBehaviour
+{
+    public Transform player;
+    public Transform targetPoint;
+    public float zoomDuration = 5f; // Duración del zoom en segundos
+    public float targetOrthoSize = 10f; // Tamaño de objetivo para el zoom
+
+    private Camera cam;
+    public float initialOrthoSize = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+       
+        cam.transform.LookAt(player);
+        StartCoroutine(ZoomOutAndRotate());
+    }
+
+    IEnumerator ZoomOutAndRotate()
+    {
+        float elapsedTime = 0f;
+        Vector3 initialPosition = transform.position;
+        Quaternion initialRotation = transform.rotation;
+        Quaternion targetRotation = Quaternion.LookRotation(targetPoint.position - transform.position);
+
+        while (elapsedTime < zoomDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / zoomDuration;
+
+            // Zoom
+            cam.orthographicSize = Mathf.Lerp(initialOrthoSize, targetOrthoSize, t);
+
+            // Rotación
+            transform.rotation = Quaternion.Slerp(initialRotation, targetRotation, t);
+
+            yield return null;
+        }
+    }
+}
