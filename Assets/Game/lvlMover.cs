14157b27b8e66cfcec80975c2ee75f330fa61513
Carles <cmarfem@upv.edu.es>
2023-06-17 14:11:24 +0200

    Botón de salida, asi como errores en el carrousel
    Se ha agregado el botón de salida y se han reparado los errores del carrousel

--- Assets/Game/lvlMover.cs
+++ Assets/Game/lvlMover.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class lvlMover : MonoBehaviour
 {
@@ -10,6 +11,25 @@ public class lvlMover : MonoBehaviour
     public int currentLevel = 0;
     public float moveDistance = 10.0f; // La distancia que se moverá el carrusel de niveles
     public float moveTime = 1.0f; // El tiempo que tardará en moverse el carrusel de niveles
+    public Text levelText; // El objeto de texto que muestra el nivel actual
+    public int maxLevels = 3;
+    public List<GameObject> objectsToDisable; // La lista de objetos que se desactivarán mientras el carrusel está en movimiento
+
+    public AudioClip clip;
+    private AudioSource audioSource;
+    private void Start()
+    {
+        levelText.text = (currentLevel + 1).ToString();
+
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            // Si no hay AudioSource adjunto al objeto, lo agregamos.
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.playOnAwake = false;
+    }
 
     void Update()
     {
@@ -20,6 +40,7 @@ public class lvlMover : MonoBehaviour
 
             if (Physics.Raycast(ray, out hit))
             {
+                audioSource.PlayOneShot(clip);
                 if (hit.transform.name == "FI")
                 {
                     // Mover al nivel anterior
@@ -36,6 +57,12 @@ public class lvlMover : MonoBehaviour
 
     IEnumerator MoveToPreviousLevel()
     {
+        // Desactiva los objetos
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(false);
+        }
+
         // Comprueba si ya estamos en el primer nivel
         if (currentLevel > 0)
         {
@@ -51,17 +78,43 @@ public class lvlMover : MonoBehaviour
             }
 
             levelCarousel.transform.position = targetPosition;
-            currentLevel--;
+            if (currentLevel >= 1)
+            {
+                currentLevel--;
+            }
+
+            // Actualiza el texto del nivel
+            levelText.text = (currentLevel + 1).ToString();
+        }
+
+        // Reactiva los objetos
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(true);
         }
     }
 
     IEnumerator MoveToNextLevel()
     {
-        // Comprueba si ya estamos en el último nivel
-        if (currentLevel < levelCarousel.transform.childCount - 1)
+        // Desactiva los objetos
+        foreach (GameObject obj in objectsToDisable)
         {
+            obj.SetActive(false);
+        }
+
+        // Comprueba si ya estamos en el último nivel
+        if (currentLevel < maxLevels - 1) // Cambiado para limitar a nivel 3
+        {
+            if (currentLevel < maxLevels - 1)
+            {
+                currentLevel++;
+            }
+
+            // Actualiza el texto del nivel
+            levelText.text = (currentLevel + 1).ToString();
+
             Vector3 startPosition = levelCarousel.transform.position;
-            Vector3 targetPosition = (currentLevel == levelCarousel.transform.childCount - 2) ? levelFinalPosition.transform.position : startPosition + Vector3.left * moveDistance;
+            Vector3 targetPosition = (currentLevel == 2) ? levelFinalPosition.transform.position : startPosition + Vector3.left * moveDistance; // Cambiado para limitar a nivel 3
             float elapsedTime = 0;
 
             while (elapsedTime < moveTime)
@@ -72,7 +125,13 @@ public class lvlMover : MonoBehaviour
             }
 
             levelCarousel.transform.position = targetPosition;
-            currentLevel++;
+
+        }
+
+        // Reactiva los objetos
+        foreach (GameObject obj in objectsToDisable)
+        {
+            obj.SetActive(true);
         }
     }
 }
