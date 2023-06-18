53da255a30553e9f148dec466f7cfe79f260f069
Arnau <clavileno3000@gmail.com>
2023-06-18 19:43:50 +0200

    Implementada Música en los menus
    - Se ha implementado nueva música para los menús (Main, Selección de niveles, Tienda y Ajustes)
    - El script usa un patrón Singleton

---
+++ Assets/Game/scripts/MusicController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class MusicController : MonoBehaviour
+{
+  public List<int> BNqueSuene = new List<int>(); //Build Niveles que quiero que Suene
+  static MusicController instance_Music;
+  public static MusicController Instance { get => instance_Music; }
+  //---------------------------------------------------------------------------------------------------------------
+  private void Update()
+  {
+    int escena = SceneManager.GetActiveScene().buildIndex;
+    if (!BNqueSuene.Contains(escena))
+    { DestroyImmediate(gameObject); }
+  }
+  //---------------------------------------------------------------------------------------------------------------
+  private void Awake()
+  {
+    if (instance_Music == null)
+    {
+      instance_Music = this;
+      DontDestroyOnLoad(gameObject);
+    }
+    else
+    {
+       DestroyImmediate(gameObject);
+    }
+  }
+}
