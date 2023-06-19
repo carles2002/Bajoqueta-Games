6312700e24e818c4cfb9d170366b680cd339708f
Arnau <clavileno3000@gmail.com>
2023-06-19 12:07:11 +0200

    Arreglo de Sonido al Borrar todos los PlayerPrefs
    - Se ha arreglado un inconveniente que afectaba a la música y SFX cuando se borraban todos los PlayerPrefs

--- Assets/Levels/Scripts/DeletePlayerPrefs.cs
+++ Assets/Levels/Scripts/DeletePlayerPrefs.cs
@@ -2,9 +2,18 @@
 
 public class DeletePlayerPrefs : MonoBehaviour
 {
+    private const string VolumeKey = "volume";
+    private const string SFXKey = "sfx";
+    private const float DefaultVolume = 1f;
     public void DeletePlayerPrefabs()
     {
         PlayerPrefs.DeleteAll();
         Debug.Log("PlayerPrefs eliminados");
+
+        float volume = PlayerPrefs.GetFloat(VolumeKey);
+        float sfx = PlayerPrefs.GetFloat(SFXKey);
+
+        PlayerPrefs.SetFloat(VolumeKey, DefaultVolume);
+        PlayerPrefs.SetFloat(SFXKey, DefaultVolume);
     }
 }
